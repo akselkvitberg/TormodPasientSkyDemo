23619289875cb06b7e4736ec79089a929e6bb0c6
Aksel Kvitberg <akv@locus.no>
2020-02-18 18:40:47 +0100

    Working api and app

---
+++ Pasientsky.WebApi/Controllers/PatientController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pasientsky.WebApi.Controllers
+{
+    [ApiController]
+    [Route("pasient")]
+    public class PatientController : ControllerBase
+    {
+        
+        List<Patient> patientList = new List<Patient>()
+        {
+            new Patient(){Id = 1, FirstName = "Ola", LastName = "Normann"},
+            new Patient(){Id = 2, FirstName = "Kari", LastName = "Knutsdottir"},
+        };
+        
+        [HttpGet]
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return patientList;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Patient Get(int id)
+        {
+            return patientList.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public Patient Add(Patient patient)
+        {
+            patient.Id = patientList.Max(x => x.Id)+1;
+            patientList.Add(patient);
+            return patient;
+        }
+
+        [HttpPut]
+        public Patient Update(Patient patient)
+        {
+            var original = patientList.FirstOrDefault(x => x.Id == patient.Id);
+            if (original == null)
+            {
+                return null;
+            }
+
+            original.FirstName = patient.FirstName;
+            original.LastName = patient.LastName;
+            return original;
+
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var patient = patientList.FirstOrDefault(x => x.Id == id);
+            if (patient != null)
+            {
+                patientList.Remove(patient);
+            }
+        }
+    }
+
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Pasientsky.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pasientsky.WebApi.Controllers;
+
+namespace Pasientsky.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _dummy;
+        private Patient _selectedPatient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PatientList = new ObservableCollection<Patient>()
+            {
+                new Patient(){Id = 1, FirstName = "Ola", LastName = "Normann"},
+                new Patient(){Id = 2, FirstName = "Kari", LastName = "Knutsdottir"},
+            };
+            DataContext = this;
+        }
+
+        public ObservableCollection<Patient> PatientList { get; set; }
+
+        public Patient SelectedPatient
+        {
+            get => _selectedPatient;
+            set { _selectedPatient = value; OnPropertyChanged();}
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
